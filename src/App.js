d263f8c753ff1bbcab46adbbfde938419c889ccf
ateeb <syedateebulislam@gmail.com>
2021-06-30 18:20:44 +0530

    Post request-L43

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import PostList from './Components/PostList';
+import PostForm from './Components/PostForm';
 
 function App() {
   return (
     <div className="App">
 
-      <PostList/>
+      {/*<PostList/>*/}
+      <PostForm/>
       
     </div>
   );
